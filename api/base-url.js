3c59279d03fe49631422068f3d2349527a8b8c18
SaintKai <375287330@qq.com>
2025-01-03 15:42:14 +0800

    分离开发与生产环境连接端口；

--- api/base-url.js
+++ api/base-url.js
@@ -6,23 +6,28 @@
  */
 module.exports = (() => {
 
-	/* aliyun */
-	const http = "https://119.23.51.178:26400";
-	const ws = "wss://119.23.51.178:26400";
-
 	//MuMu模拟器IP 10.0.2.2 对应电脑IP 127.0.0.1
 
 	/* .NET6.0 WebAPI */
-	// const http = "https://10.0.2.2:7275";
-	// const ws = "wss://10.0.2.2:7275";
+	// let http = "https://10.0.2.2:7275";
+	// let ws = "wss://10.0.2.2:7275";
 
 	/* .NET6.0 WebAPI */
-	// const http = "http://192.168.110.105:5244";
-	// const ws = "ws://192.168.110.105:5244";
+	// let http = "http://10.0.2.2:5244";
+	// let ws = "ws://10.0.2.2:5244";
 
-	/* Node Express */
-	// const http = "https://192.168.110.105:26400";
-	// const ws = "wss://192.168.110.105:26400";
+	/* aliyun */
+	// let http = "https://127.0.0.1:26400";
+	// let ws = "wss://127.0.0.1:26400";
+
+	/* TencentCloud */
+	let http = "https://127.0.0.1:26400";
+	let ws = "wss://127.0.0.1:26400";
+
+	if (process.env.NODE_ENV === "development") {
+		http = http.replace("26400", "25500");
+		ws = ws.replace("26400", "25500");
+	}
 
 	let file = `${http}/File/GetFile?fileId=`;
 	const userInfo = uni.getStorageSync("userInfo");
